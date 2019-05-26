808e7442a0244a283cb2cf59e5938a2ef7ddf230
Gleyder <gleyderh@gmail.com>
2019-05-26 23:22:40 +0200

    fixes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
-import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
@@ -11,43 +9,18 @@ import pickle
 
 start = time.time()
 
-clasificador = pickle.load(open("model.p", "rb"))
-
 # Import Datasets
-df_seq = pd.read_csv('pdb_data_seq-1.csv')
-df_char = pd.read_csv('pdb_data_no_dups.csv')
-
-print('Datasets have been loaded...')
-
-# Filter for only proteins
-protein_char = df_char[df_char.macromoleculeType == 'Protein']
-protein_seq = df_seq[df_seq.macromoleculeType == 'Protein']
-
-# Select only necessary variables to join
-protein_char = protein_char[['structureId','classification']]
-protein_seq = protein_seq[['structureId','sequence']]
-
-# Join two datasets on structureId
-model_f = protein_char.set_index('structureId').join(protein_seq.set_index('structureId'))
-
-
-model_f.isnull().sum()
-
-# Drop rows with missing values
-model_f = model_f.dropna()
+df_data = pd.read_csv('dataset.csv')
 
 # Look at classification type counts
-counts = model_f.classification.value_counts()
+counts = df_data.classification.value_counts()
 
 
 # Get classification types where counts are over 1000
 types = np.asarray(counts[(counts > 1000)].index)
 
 # Filter dataset's records for classification types > 1000
-data = model_f[model_f.classification.isin(types)]
-
-# print(types)
-# print('%d is the number of records in the final filtered dataset' %data.shape[0])
+data = df_data[df_data.classification.isin(types)]
 
 
 # 3). ----- Train Test Split -----
@@ -60,19 +33,27 @@ vect = CountVectorizer(analyzer = 'char_wb', ngram_range = (4,4))
 
 # Fit and Transform CountVectorizer
 vect.fit(X_train)
+X_train_df = vect.transform(X_train)
 X_test_df = vect.transform(X_test)
 
-# # Make a prediction dictionary to store accuracys
+# 4). ------ Machine Learning Models ------
+
+# Make a prediction dictionary to store accuracys
 prediction = dict()
-NB_pred = clasificador.predict(X_test_df)
+
+# Naive Bayes Model
+model = MultinomialNB()
+model.fit(X_train_df, y_train)
+NB_pred = model.predict(X_test_df)
 prediction["MultinomialNB"] = accuracy_score(NB_pred, y_test)
 print( prediction['MultinomialNB'])
 
-# #Print F1 score metrics
+#Print F1 score metrics
 print(classification_report(y_test, NB_pred, target_names = types))
+
 
 end = time.time()
 
 print("%s minutos" % (str((end - start) / 60)))
 
-
+pickle.dump(model, open("model.p", "wb"))
