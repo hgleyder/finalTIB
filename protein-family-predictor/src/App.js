c1c62b5b77fff912a4495dd25b8b366fe36cec89
Gleyder <gleyderh@gmail.com>
2019-05-28 22:52:08 +0200

    site ready

--- protein-family-predictor/src/App.js
+++ protein-family-predictor/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Box, Button, TextInput, Chart, Grommet } from 'grommet';
+import { Box, Button, TextInput, Grommet } from 'grommet';
 import styled from 'styled-components';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import ProteinService from './service/backend'
+import Logo from './assets/logo.png'
+import BackgroundImage from './assets/bg.jpg'
 
 
 const theme = {
@@ -21,13 +23,14 @@ class App extends React.Component {
     protein: '',
     results: '',
     showResults: false,
+    loading: false,
   }
 
   clasificarProteina = async () => {
     const {protein} = this.state;
+    this.setState({loading: true})
     const results = await ProteinService.clasificaProteina(protein);
-    console.log(results.data)
-    this.setState({ results: results.data, showResults: true });
+    this.setState({ results: results.data, showResults: true, loading: false });
   }
 
 
@@ -35,7 +38,7 @@ renderLineChart =  () => {
   const  {results } = this.state;
   const data = results.probabilidades[0].map((p, index) => ({probabilidad: p, name: results.clases[index]}))
   return (
-  <LineChart width={1400} height={600} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+  <LineChart width={1400} height={500} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
     <Line type="monotone" dataKey="probabilidad" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
     <XAxis dataKey="name" />
@@ -44,28 +47,36 @@ renderLineChart =  () => {
   </LineChart>
 )};
   render(){
-    const {protein, showResults,} = this.state;
+    const {protein, showResults, loading} = this.state;
   return (
     <Grommet theme={theme}>
+      <Background>
       <Box align="center" height="100vh">
         <Container>
+        <img src={Logo} className="logo" alt="logo" />
           <TextInput
             placeholder="Escribe aqui la secuencia de aminoacidos"
             value={protein}
             onChange={event => this.setState({protein: event.target.value})}
           />
           <Button
-            label="Clasificar Proteina"
+            label={loading? "Cargando..." : "Clasificar Proteina"}
             primary 
+            disabled={loading}
             onClick={this.clasificarProteina}
           />
           {showResults && this.renderLineChart()}
         </Container>
       </Box>
+      </Background>
      </Grommet>
   );
   }
 }
+
+const Background = styled.div`
+  background-image: url(${BackgroundImage});
+`;
 
 const Container = styled.div`
   display: flex;
@@ -82,6 +93,11 @@ const Container = styled.div`
 
   & > .recharts-wrapper {
     margin-top: 2rem;
+    margin-left: -3rem;
+  }
+  & > .logo{
+    width: 40%;
+    margin-top: -8rem;
   }
 `;
 
